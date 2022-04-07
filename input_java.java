0922a9fd57eda29ba4a38ecc7e51961ac4e1b8dc
Kartik6101 <102985620+Kartik6101@users.noreply.github.com>
2022-04-08 01:11:47 +0530

    Add files via upload

---
+++ input_java.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class input_java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			
+	Scanner	sc = new Scanner(System.in);
+	System.out.println("Input Your Age");
+	int age = sc.nextInt();
+	System.out.println(age);
+	
+	}
+
+}
